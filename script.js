8c2779a6253aa4628df38c7a65258ce64c2af628
Blaise Mushimiyumukiza <127123955+ReponseBlaise@users.noreply.github.com>
2025-12-09 14:42:34 +0000

    hh

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function changeBackgroundColor(color){
-    let section = document.getElementById('coding-journey');
-    section.style.backgroundColor = color;
+  let section = document.getElementById('journey');
+  section.style.backgroundColor = color;
 }
 
 document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
-
 document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
-
 document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+
+// Dark/Light Mode Toggle
+document.getElementById('modeToggle').addEventListener('click', () => {
+  document.body.classList.toggle("dark-mode");
+});
